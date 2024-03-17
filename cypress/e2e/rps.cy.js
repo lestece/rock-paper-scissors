6516c9870f0de4aa2ed1c46456f5b3d7b81a4ae3
Celeste Loi <loiceleste94@gmail.com>
2024-03-17 17:23:41 +0100

    Test the Paper button

--- cypress/e2e/rps.cy.js
+++ cypress/e2e/rps.cy.js
@@ -30,15 +30,28 @@ describe("RPS game", () => {
   });
   // Play the game with the 'Paper' option and check the result
   it("play the game with paper option and check the result", () => {
-    //Click the 'Rock' button should exist
+    //Click the 'Paper' button should exist
+    cy.get('[data-cy="paper"]').should("exist").click();
     //Check that the user option is set to 'Paper'
+    cy.get('[data-cy="user-option"]').should("have.text", "Paper");
     //Check the computer option
-    //If the computer option is set to 'Rock'
-    //the result is user win
-    //If the computer option is set to 'Paper'
-    //the result is a tie
-    //If the computer option is set to 'Scissors'
-    //the result is a computer win
+    cy.get('[data-cy="computer-option"]').then((option) => {
+      //If the computer option is set to 'Rock'
+      if (option.text().includes("Rock")) {
+        //the result is user win
+        cy.get('[data-cy="result"]').contains("You win!");
+      }
+      //If the computer option is set to 'Paper'
+      else if (option.text().includes("Paper")) {
+        //the result is a tie
+        cy.get('[data-cy="result"]').contains("It's a tie!");
+      }
+      //If the computer option is set to 'Scissors'
+      else {
+        //the result is a computer win
+        cy.get('[data-cy="result"]').contains("The computer wins, you lose!");
+      }
+    });
   });
   // Play the game with the 'Scissors' option and check the result
   it("play the game with scissors option and check the result", () => {
